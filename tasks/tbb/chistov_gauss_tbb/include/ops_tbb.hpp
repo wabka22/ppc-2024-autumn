75a22758a65d7cf939283a193ae35150beb2da3f
Alexey Chistov <alexey.chistov10@gmail.com>
2025-04-04 11:06:02 +0300

    first try

---
+++ tasks/tbb/chistov_gauss_tbb/include/ops_tbb.hpp
@@ -0,0 +1,61 @@
+#ifndef TASKS_EXAMPLES_TEST_TBB_OPS_TBB_H_
+#define TASKS_EXAMPLES_TEST_TBB_OPS_TBB_H_
+
+#include <string>
+#include <vector>
+#include <functional>
+#include <numeric>
+#include <random>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace chistov_gauss_tbb {
+
+class TestTaskTBB : public ppc::core::Task {
+ public:
+  explicit TestTaskTBB(std::shared_ptr<ppc::core::TaskData> taskData) : Task(std::move(taskData)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<double> image, result_image;
+  std::vector<double> kernel;
+  size_t height, width;
+};
+
+class TestTaskOpenMP : public ppc::core::Task {
+ public:
+  explicit TestTaskOpenMP(std::shared_ptr<ppc::core::TaskData> taskData) : Task(std::move(taskData)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<double> image, result_image;
+  std::vector<double> kernel;
+  size_t height, width;
+};
+
+class TestTaskSequential : public ppc::core::Task {
+ public:
+  explicit TestTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData) : Task(std::move(taskData)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<double> image, result_image;
+  std::vector<double> kernel;
+  size_t height, width;
+};
+
+}  // namespace chistov_gauss_tbb
+
+#endif
---
+++ tasks/tbb/chistov_gauss_tbb/src/ops_tbb.cpp
@@ -0,0 +1,163 @@
+// Copyright 2023 Nesterov Alexander
+#include "tbb/chistov_gauss_tbb/include/ops_tbb.hpp"
+
+#include <tbb/tbb.h>
+
+bool chistov_gauss_tbb::TestTaskSequential::pre_processing() {
+  kernel.assign(reinterpret_cast<double *>(taskData->inputs[1]), reinterpret_cast<double *>(taskData->inputs[1]) + 3);
+  width = static_cast<size_t>(taskData->inputs_count[1]);
+  height = static_cast<size_t>(taskData->inputs_count[2]);
+  result_image = std::vector<double>(width * height, 0);
+  return true;
+}
+
+bool chistov_gauss_tbb::TestTaskSequential::validation() {
+  if (taskData->inputs[0] == nullptr || taskData->inputs_count[0] == 0) {
+    return false;
+  }
+
+  image.assign(reinterpret_cast<double *>(taskData->inputs[0]),
+               reinterpret_cast<double *>(taskData->inputs[0]) + taskData->inputs_count[0]);
+
+  for (size_t i = 0; i < taskData->inputs_count[1] * taskData->inputs_count[2]; ++i) {
+    if (image[i] < 0 || image[i] > 255) {
+      return false;
+    }
+  }
+
+  return taskData->inputs_count[0] > 0 &&
+         taskData->inputs_count[0] == taskData->inputs_count[1] * taskData->inputs_count[2] &&
+         taskData->outputs_count[0] == taskData->inputs_count[1] * taskData->inputs_count[2] &&
+         taskData->inputs_count[1] >= 3 && taskData->inputs_count[2] >= 3;
+}
+
+bool chistov_gauss_tbb::TestTaskSequential::run() {
+  double sum = kernel[0] + kernel[1] + kernel[2];
+  for (size_t i = 0; i < height; ++i) {
+    for (size_t j = 0; j < width; ++j) {
+      double value = 0.0;
+      for (ptrdiff_t k = -1; k <= 1; ++k) {
+        ptrdiff_t tmp = static_cast<ptrdiff_t>(j) + k;
+
+        if (tmp >= 0 && tmp < static_cast<ptrdiff_t>(width)) {
+          value += image[(i * width) + tmp] * kernel[k + 1];
+        }
+      }
+
+      result_image[(i * width) + j] = value / sum;
+    }
+  }
+
+  return true;
+}
+
+bool chistov_gauss_tbb::TestTaskSequential::post_processing() {
+  std::copy(result_image.begin(), result_image.end(), reinterpret_cast<double *>(taskData->outputs[0]));
+  return true;
+}
+
+bool chistov_gauss_tbb::TestTaskOpenMP::pre_processing() {
+  kernel.assign(reinterpret_cast<double *>(taskData->inputs[1]), reinterpret_cast<double *>(taskData->inputs[1]) + 3);
+  width = static_cast<size_t>(taskData->inputs_count[1]);
+  height = static_cast<size_t>(taskData->inputs_count[2]);
+  result_image = std::vector<double>(width * height, 0);
+  return true;
+}
+
+bool chistov_gauss_tbb::TestTaskOpenMP::validation() {
+  if (taskData->inputs[0] == nullptr || taskData->inputs_count[0] == 0) {
+    return false;
+  }
+
+  image.assign(reinterpret_cast<double *>(taskData->inputs[0]),
+               reinterpret_cast<double *>(taskData->inputs[0]) + taskData->inputs_count[0]);
+
+  for (size_t i = 0; i < taskData->inputs_count[1] * taskData->inputs_count[2]; ++i) {
+    if (image[i] < 0 || image[i] > 255) {
+      return false;
+    }
+  }
+
+  return taskData->inputs_count[0] > 0 &&
+         taskData->inputs_count[0] == taskData->inputs_count[1] * taskData->inputs_count[2] &&
+         taskData->outputs_count[0] == taskData->inputs_count[1] * taskData->inputs_count[2] &&
+         taskData->inputs_count[1] >= 3 && taskData->inputs_count[2] >= 3;
+}
+
+bool chistov_gauss_tbb::TestTaskOpenMP::run() {
+  double sum_inv = 1.0 / (kernel[0] + kernel[1] + kernel[2]);
+  int h = static_cast<int>(height);
+  int w = static_cast<int>(width);
+
+#pragma omp parallel firstprivate(sum_inv) shared(w, h) num_threads(8)
+  {
+#pragma omp for
+    for (int i = 0; i < h; ++i) {
+      for (int j = 0; j < w; ++j) {
+        double pixel_0 = (j > 0) ? image[i * width + (j - 1)] * kernel[0] : 0.0;
+        double pixel_1 = image[i * width + j] * kernel[1];
+        double pixel_2 = (j < width - 1) ? image[i * width + (j + 1)] * kernel[2] : 0.0;
+
+        result_image[i * width + j] = (pixel_0 + pixel_1 + pixel_2) * sum_inv;
+      }
+    }
+  }
+
+  return true;
+}
+
+bool chistov_gauss_tbb::TestTaskOpenMP::post_processing() {
+  std::copy(result_image.begin(), result_image.end(), reinterpret_cast<double *>(taskData->outputs[0]));
+  return true;
+}
+
+ bool chistov_gauss_tbb::TestTaskTBB::pre_processing() {
+  kernel.assign(reinterpret_cast<double *>(taskData->inputs[1]), reinterpret_cast<double *>(taskData->inputs[1]) +
+  3); width = static_cast<size_t>(taskData->inputs_count[1]); height =
+  static_cast<size_t>(taskData->inputs_count[2]); result_image = std::vector<double>(width * height, 0); return true;
+}
+
+ bool chistov_gauss_tbb::TestTaskTBB::validation() {
+  if (taskData->inputs[0] == nullptr || taskData->inputs_count[0] == 0) {
+    return false;
+  }
+
+  image.assign(reinterpret_cast<double *>(taskData->inputs[0]),
+             reinterpret_cast<double *>(taskData->inputs[0]) + taskData->inputs_count[0]);
+
+  for (size_t i = 0; i < taskData->inputs_count[1] * taskData->inputs_count[2]; ++i) {
+    if (image[i] < 0 || image[i] > 255) {
+      return false;
+    }
+  }
+
+  return taskData->inputs_count[0] > 0 &&
+         taskData->inputs_count[0] == taskData->inputs_count[1] * taskData->inputs_count[2] &&
+         taskData->outputs_count[0] == taskData->inputs_count[1] * taskData->inputs_count[2] &&
+         taskData->inputs_count[1] >= 3 && taskData->inputs_count[2] >= 3;
+}
+
+ bool chistov_gauss_tbb::TestTaskTBB::run() {
+  double sum_inv = 1.0 / (kernel[0] + kernel[1] + kernel[2]);
+  int h = static_cast<int>(height);
+  int w = static_cast<int>(width);
+
+  tbb::parallel_for(tbb::blocked_range2d<int>(0, h, 0, w), [&](const tbb::blocked_range2d<int> &r) {
+    for (int i = r.rows().begin(); i < r.rows().end(); ++i) {
+      for (int j = r.cols().begin(); j < r.cols().end(); ++j) {
+        double pixel_0 = (j > 0) ? image[i * width + (j - 1)] * kernel[0] : 0.0;
+        double pixel_1 = image[i * width + j] * kernel[1];
+        double pixel_2 = (j < width - 1) ? image[i * width + (j + 1)] * kernel[2] : 0.0;
+
+        result_image[i * width + j] = (pixel_0 + pixel_1 + pixel_2) * sum_inv;
+      }
+    }
+  });
+
+  return true;
+}
+
+ bool chistov_gauss_tbb::TestTaskTBB::post_processing() {
+  std::copy(result_image.begin(), result_image.end(), reinterpret_cast<double *>(taskData->outputs[0]));
+  return true;
+}
